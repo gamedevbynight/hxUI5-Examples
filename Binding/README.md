5782b625d0ae1e422c2a49ce29f93f3d27091126
gamedevbynight <nicolas.korbel@outlook.com>
2019-07-08 21:48:53 +0200

    Binding example

--- Binding/README.md
+++ Binding/README.md
@@ -1,1 +1,3 @@
 # Binding example
+
+For more infos on binding data in UI5 visit [UI5 Documentation](https://openui5.hana.ondemand.com/#/topic/68b9644a253741e8a4b9e4279a35c247)
--- Binding/src/Main.hx
+++ Binding/src/Main.hx
@@ -1,5 +1,74 @@
+import sap.ui.model.json.JSONModel;
+
 class Main {
 	static function main() {
-		trace("Hello, world!");
+		new Main();
+	}
+
+	public function new()
+	{
+		var employees:Array<Employee> = new Array<Employee>();
+		employees.push(new Employee("Peter", "Barker", 38));
+		employees.push(new Employee("Alex", "Keyran", 25));
+		employees.push(new Employee("Christie", "Davin", 56));
+
+		var model:JSONModel = new JSONModel(employees);
+
+		var list = new sap.m.List({
+	
+		});
+
+		list.bindItems({
+			path: "/",
+			template: new sap.m.StandardListItem({
+				title: '{firstName}',
+				description: '{lastName}',
+				counter: '{age}'
+			})
+		});
+
+		var object = new SomeObject(399);
+
+
+		var page = new sap.m.Page({
+			content: [list, new sap.m.Text({
+				text: 'Objects: {myObject>/objectCount}'
+			})]
+		});
+
+		page.setModel(model);
+		page.setModel(new JSONModel(object), "myObject");
+
+		var app = new sap.m.App({
+			pages: [page],
+			initialPage: page
+		});
+
+		app.placeAt("content");
+
+	}
+}
+
+class Employee
+{
+	public var firstName:String;
+	public var lastName:String;
+	public var age:Int;
+
+	public function new(firstName:String, lastName:String,age:Int)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+}
+
+class SomeObject
+{
+	public var objectCount:Int;
+
+	public function new(objectCount:Int)
+	{
+		this.objectCount = objectCount;
 	}
 }
